f01084e09a612f02bdb8ee7537cc17de087540a0
RJGJ <jaictinrj@gmail.com>
2022-01-20 17:23:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hyperhidrosis
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hyperhidrosis)
---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+</script>
+
+<template>
+  <carousel>
+    <slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">
+        <h2>Slide {{ slide }}</h2>
+      </div>
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
